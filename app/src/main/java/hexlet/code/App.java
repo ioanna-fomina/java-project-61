c579d90cf7b77ef8773eede38c8c52c3e7429229
Ioanna Fomina <ioanna.fomina.software@gmail.com>
2023-01-20 13:17:33 +0300

    add Greeting User in game

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,12 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Welcome to the Brain Games!");
+        Cli.greeting(scanner);
     }
 }
---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,19 @@
+package hexlet.code;
+
+import java.util.Scanner;
+public class Cli {
+    Scanner scanner;
+
+    public Cli(Scanner scanner) {
+        this.scanner = scanner;
+    }
+    public static void greeting(Scanner scanner) {
+        //Scanner scanner = new Scanner(System.in);
+
+        System.out.print("May I have your name? ");
+        String userName = scanner.next();
+        System.out.println("Hello, " + userName + "!");
+
+        scanner.close();
+    }
+}
