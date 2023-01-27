a4c9d46b402247705a1c03eb63ce6fa5a5fee306
Ioanna Fomina <ioanna.fomina.software@gmail.com>
2023-01-27 21:05:02 +0300

    add method of finding the GCD in Utils.java

--- app/src/main/java/hexlet/code/service/Utils.java
+++ app/src/main/java/hexlet/code/service/Utils.java
@@ -10,4 +10,32 @@ public class Utils {
         int i = new Random().nextInt(array.length);
         return array[i];
     }
+    public static int gcdOfNumbers(int n1, int n2) {
+        if (n1 == 0) {
+            return n2;
+        }
+        if (n2 == 0) {
+            return n1;
+        }
+        int n;
+        for (n = 0; ((n1 | n2) & 1) == 0; n++) {
+            n1 >>= 1;
+            n2 >>= 1;
+        }
+        while ((n1 & 1) == 0) {
+            n1 >>= 1;
+        }
+        do {
+            while ((n2 & 1) == 0) {
+                n2 >>= 1;
+            }
+            if (n1 > n2) {
+                int temp = n1;
+                n1 = n2;
+                n2 = temp;
+            }
+            n2 = (n2 - n1);
+        } while (n2 != 0);
+        return n1 << n;
+    }
 }
